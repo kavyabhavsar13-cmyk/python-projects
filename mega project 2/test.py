6f0dff5222b7faee33b10bd384f529f7389a560b
kavyabhavsar13-cmyk <kavyabhavsar13@gmail.com>
2026-02-08 20:36:19 +0530

    shift to uv

---
+++ mega project 2/test.py
@@ -0,0 +1,9 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Say something...")
+    audio = r.listen(source)
+
+print("You said:", r.recognize_google(audio))
